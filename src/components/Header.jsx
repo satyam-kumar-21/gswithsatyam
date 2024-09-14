d6565a07a18b79fbc8a31f5bbb9397784f8fc6e1
Satyam kumar <satyamkumar2716@gmail.com>
2024-09-14 15:56:18 +0530

    first day

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,8 +34,8 @@ export default function Header() {
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
+            <span className="">GS WITH SATYAM</span>
+            {/* <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" /> */}
           </Link>
         </div>
         <div className="flex lg:hidden">
